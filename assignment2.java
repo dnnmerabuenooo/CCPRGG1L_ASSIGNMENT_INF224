5b395d93ed7a2917b9f83437c3979bc9cde27897
Dianne Merabueno <111546446+dnnmerabuenooo@users.noreply.github.com>
2022-10-18 18:33:41 +0800

    Create assignment2.java

---
+++ assignment2.java
@@ -0,0 +1,40 @@
+public class assignment2 {
+    
+public static void main(String[] args) throws Exception{
+    
+    // 1. Use a while loop to print your FIRST NAME three times.
+    int count1 = 3;
+    
+    while (count1 != 0){
+        System.out.println("Toni Dianne");
+        count1--;
+    }
+  
+    // 2. Use a do-while loop to print your MIDDLE NAME four times.
+    int count2 = 0;
+    
+    do {
+        System.out.println("Guarina");
+        count2++;
+    } while (count2 != 4);
+  
+    // 3. Use a for loop to print your SURNAME five times.
+    for (int count3 = 5; count3 != 0; count3--){
+        System.out.println("Merabueno");
+    }
+  
+    // 4. Create a char array that consists all of the characters of your FIRST NAME and use a for loop to print each character of your FIRST NAME.
+    String[] firstnameCharacters = { "T", "O", "N", "I", "D", "I", "A", "N", "N", "E" };
+    
+    for (int count4 = 0; count4 < firstnameCharacters.length; count4++){
+        System.out.println(firstnameCharacters[count4]);
+    }
+    
+    // 5. Use a for loop to print the char array of your FIRST NAME in REVERSE order.
+    String[] firstnameReverse = { "E", "N", "N", "A", "I", "D", "I", "N", "O", "T" };
+        
+    for (int count5 = 0; count5 < firstnameReverse.length; count5++){
+        System.out.println(firstnameReverse[count5]);
+    }
+}    
+}
